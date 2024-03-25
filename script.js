5b717546be8c0e7b2aa5fff4e268fc2340f1d42c
Alok Gadwal <86949808+alokgadwal26@users.noreply.github.com>
2024-03-25 18:38:40 +0530

    Add files via upload
    To Do List App File Upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("listContainer");
+const button = document.getElementById("addButton");
+const date = document.getElementById("date");
+const time = document.getElementById("time");
+
+button.addEventListener('click', addtask )
+
+inputBox.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    addtask();
+  }
+});
+
+function addtask(){
+    if (inputBox.value === "") {
+        alert("You Must Write Somthing!");
+      } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+      }
+    
+      inputBox.value = "";
+      saveDate();
+}
+
+
+
+
+
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveDate()
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveDate()
+  }
+});
+
+function saveDate() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+
+showTask();
